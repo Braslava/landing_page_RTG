45cf2ae601513c725fdb38ad5fe226968f1392de
Mara Braslava <m.braslava@gmail.com>
2022-04-10 22:03:48 +0300

    adding questions as a data array

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
-const faqContainer = document.querySelector(".faq");
+//import {faqData} from "./faq-data";
 
+const faqData = [
+    {
+        question: "Lorem ipsum dolor sit, ametconsectetur adipisicing?",
+        answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quibusdam? Nihil cumque labore ad, delectus nulla, nesciunt voluptate a natus asperiores exercitationem soluta fuga, provident omnis aliquid. Sit accusamus maiores quidem magni id culpa voluptas sint. Odit voluptatem corrupti id ex autnemo officiis officia in.",
+    },
+    {
+        question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, quis quas tempore, nihil maxime temporibus debitis nemo facere nostrum id eligendi? Illum eaque atque fugit provident, voluptate sed facere officiis voluptatum assumenda tempora eius neque commodi aperiam dolor ipsa in id minima! Eius dicta nesciunt est cum consequatur nemo fuga?",
+    },
+    {
+        question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, quis quas tempore, nihil maxime temporibus debitis nemo facere nostrum id eligendi? Illum eaque atque fugit provident, voluptate sed facere officiis voluptatum assumenda tempora eius neque commodi aperiam dolor ipsa in id minima! Eius dicta nesciunt est cum consequatur nemo fuga?",
+    },
+    {
+        question: "Lorem ipsum dolor sit amet consectetur adipisicing elit?",
+        answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos, quis quas tempore, nihil maxime temporibus debitis nemo facere nostrum id eligendi? Illum eaque atque fugit provident, voluptate sed facere officiis voluptatum assumenda tempora eius neque commodi aperiam dolor ipsa in id minima! Eius dicta nesciunt est cum consequatur nemo fuga?",
+    },
+    {
+        question: "What dishes are your speciality?",
+        answer: `<p>We serve:</p>
+        <ul class="list">
+            <li>Pastas</li>
+            <li>Burgers (including vegan and vegetarian)</li>
+            <li>Salads</li>
+            <li>Rissotos</li>
+            <li>Cakes and desserts</li>
+        </ul>`,
+    },
+    {
+        question: `Where can I see the menu?`,
+        answer: `You can find the menu right here &#8594
+        <span class="button-sm-block"><a href="menu.html#menu" class="button button--link ">Menu</a></span>`,
+    },
+];
+console.log(faqData);
+
+const faqContainer = document.querySelector(".faq");
 //const questionContainers = document.querySelectorAll("")
 
-const handleClick = (e) => {
-    // console.log(e.target);
+// ** Handelers **
+
+const handleQuestionClick = (e) => {
     if (!e.target.matches(".question") && !e.target.matches(".chevron")) {
         return;
     }
-    console.log("clicked the question");
     const questionContainer = e.target.closest(".questionContainer");
-    console.log(questionContainer);
+    const answer = questionContainer.querySelector(".answer");
+    const chevron = questionContainer.querySelector(".chevron");
+    toggleOpenAnswer(answer, chevron);
 };
 
-faqContainer.addEventListener("click", handleClick);
+const handleCloseAll = () => {
+    console.log("close all");
+};
+
+const handleOepnAll = () => {
+    console.log("open all");
+};
+
+// ** Main functions **
+
+const toggleOpenAnswer = (answer, chevron) => {
+    chevron.classList.toggle("bottom");
+    answer.classList.toggle("hidden");
+};
+
+// ** Event listeners **
+faqContainer.addEventListener("click", handleQuestionClick);
